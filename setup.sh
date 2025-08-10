bc8274ce15194fb2ae88cea63b0cb5b13897a641
Peter Lin <peter.l@ikigai.team>
2025-08-11 03:56:56 +0800

    remove .zshrc after intalling oh-my-zsh

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ echo "Starting environment setup..."
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   echo "Installing Oh My Zsh..."
   RUNZSH=no CHSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+  rm .zshrc
 else
   echo "Oh My Zsh already installed."
 fi
